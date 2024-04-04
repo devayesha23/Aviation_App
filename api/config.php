b24b49541d13c8e6ff67991b1948dcf2362b460a
devayesha23 <zahraayesha414@gmail.com>
2024-04-04 06:47:56 +0500

    My Aviation App

--- api/config.php
+++ api/config.php
@@ -1,5 +1,5 @@
 <?php
 // Aviationstack API Configuration
-$api_key = "YOUR API KEY"; // Replace with your Aviationstack API key
+$api_key = "cbb0c533361bfe3371e04947ec7568b6"; // Replace with your Aviationstack API key
 $base_url = "http://api.aviationstack.com/v1/";
 ?>
